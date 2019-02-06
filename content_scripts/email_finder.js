df4b4e7c7f4804d9e999dbe24f6b28a797394905
Cole Smith <css@nyu.edu>
2019-02-06 12:54:09 -0500

    Added file headers and changed indentation to 4-spaces, removed trailing whitespace

--- content_scripts/email_finder.js
+++ content_scripts/email_finder.js
@@ -1,3 +1,10 @@
+/*
+ * Team12
+ * email-detector
+ * email_finder.js
+ * GPL-3.0
+ */
+
 (function() {
     /**
      * Check and set a global guard variable.
@@ -9,32 +16,32 @@
     }
     window.hasRun = true;
 
-	/**
-	 * Runs a regular expression to find emails
-	 * within a given page. Returns the strings
-	 * that adhere to the regular expression as
-	 * an array.
-	 */
-	function findEmails() {
-		// Email regex taken from:
-		// https://stackoverflow.com/questions/16424659/check-if-a-string-contains-an-email-address
-		const regex = new RegExp(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-		const { innerHTML } = document.body;
-		const emails = innerHTML.match(regex);
+    /**
+     * Runs a regular expression to find emails
+     * within a given page. Returns the strings
+     * that adhere to the regular expression as
+     * an array.
+     */
+    function findEmails() {
+        // Email regex taken from:
+        // https://stackoverflow.com/questions/16424659/check-if-a-string-contains-an-email-address
+        const regex = new RegExp(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+        const { innerHTML } = document.body;
+        const emails = innerHTML.match(regex);
 
         // Remove duplicates using set conversion
         return [...new Set(emails)];
-	}
+    }
 
-	/**
-	 * Listen for messages from the background script.
-	 * Call "find_emails()" or "reset()".
-	 */
-	browser.runtime.onMessage.addListener((message) => {
-		if (message.command === "find emails") {
-			browser.runtime.sendMessage({
-				emails: findEmails(),
-			});
-		}
-	});
+    /**
+     * Listen for messages from the background script.
+     * Call "find_emails()" or "reset()".
+     */
+    browser.runtime.onMessage.addListener((message) => {
+        if (message.command === "find emails") {
+            browser.runtime.sendMessage({
+                emails: findEmails(),
+            });
+        }
+    });
 })();
--- popup/find_emails.js
+++ popup/find_emails.js
@@ -1,3 +1,9 @@
+/*
+ * Team12
+ * email-detector
+ * find_emails.js
+ * GPL-3.0
+ */
 
 // SVG icon taken from:
 // https://www.materialui.co/icon/content-copy
@@ -7,111 +13,111 @@ const copyIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="23"
  * Listen for messages from the content script.
  */
 browser.runtime.onMessage.addListener((message) => {
-	const { emails } = message;
-	if (emails) {
+    const { emails } = message;
+    if (emails) {
 
-		// Clear any old email addresses.
-		const container = document.querySelector(".email-addresses");
-		container.innerHTML = "";
+        // Clear any old email addresses.
+        const container = document.querySelector(".email-addresses");
+        container.innerHTML = "";
 
-		/**
-		 * Builds and appends a no emails message to the DOM.
-		 */
-		function displayNoEmailMessage () {
-			const text = document.createTextNode("404: No emails found");
-			const h5 = document.createElement("h5");
-			h5.appendChild(text);
-			container.appendChild(h5);
-		}
+        /**
+         * Builds and appends a no emails message to the DOM.
+         */
+        function displayNoEmailMessage () {
+            const text = document.createTextNode("404: No emails found");
+            const h5 = document.createElement("h5");
+            h5.appendChild(text);
+            container.appendChild(h5);
+        }
 
-		/**
-		 * Builds and appends an email address div to the DOM.
-		 */
-		function displayEmailAddress(email) {
-			const input = document.createElement("input");
-			input.value = email;
-			const icon = document.createElement('div');
-			icon.classList.add('copy-icon');
-			icon.innerHTML = copyIcon;
-			const div = document.createElement('div');
-			div.classList.add('email-address');
-			div.appendChild(input);
-			div.appendChild(icon);
-			container.appendChild(div);
+        /**
+         * Builds and appends an email address div to the DOM.
+         */
+        function displayEmailAddress(email) {
+            const input = document.createElement("input");
+            input.value = email;
+            const icon = document.createElement('div');
+            icon.classList.add('copy-icon');
+            icon.innerHTML = copyIcon;
+            const div = document.createElement('div');
+            div.classList.add('email-address');
+            div.appendChild(input);
+            div.appendChild(icon);
+            container.appendChild(div);
 
-			/**
-			 * When the user clicks the copy icon, focus the
-			 * email input and copy it to their clipboard.
-			 */
-			icon.addEventListener("click", () => {
-				input.select();
-				document.execCommand("copy");
-			});
-		}
+            /**
+             * When the user clicks the copy icon, focus the
+             * email input and copy it to their clipboard.
+             */
+            icon.addEventListener("click", () => {
+                input.select();
+                document.execCommand("copy");
+            });
+        }
 
-		/**
-		 * If no emails were found, display the no emails
-		 * message. Otherwise, display the email addresses to
-		 * the user.
-		 */
-		if (!emails.length) {
-			displayNoEmailMessage();
-			return;
-		}
-		emails.forEach((email) => {
-			displayEmailAddress(email);
-		});
-	}
+        /**
+         * If no emails were found, display the no emails
+         * message. Otherwise, display the email addresses to
+         * the user.
+         */
+        if (!emails.length) {
+            displayNoEmailMessage();
+            return;
+        }
+        emails.forEach((email) => {
+            displayEmailAddress(email);
+        });
+    }
 });
 
 /**
-* Listen for clicks on the buttons, and send the appropriate message to
-* the content script in the page.
-*/
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
 function listenForClicks() {
-	const submit = document.querySelector(".js-submit");
-	submit.addEventListener("click", (e) => {
+    const submit = document.querySelector(".js-submit");
+    submit.addEventListener("click", (e) => {
 
-		/**
-		 * Sends a command to the content script to get all
-		 * email addresses from the DOM.
-		 * We can expect a response containing an array of
-		 * email addresses.
-		 */
-		function findEmails(tabs) {
-			browser.tabs.sendMessage(tabs[0].id, {
-				command: "find emails",
-			});
-		}
+        /**
+         * Sends a command to the content script to get all
+         * email addresses from the DOM.
+         * We can expect a response containing an array of
+         * email addresses.
+         */
+        function findEmails(tabs) {
+            browser.tabs.sendMessage(tabs[0].id, {
+                command: "find emails",
+            });
+        }
 
-		/**
-		* Just log the error to the console.
-		*/
-		function reportError(error) {
-			console.error(`Find Emails Failed: ${error}`);
-		}
+        /**
+         * Just log the error to the console.
+         */
+        function reportError(error) {
+            console.error(`Find Emails Failed: ${error}`);
+        }
 
-		/**
-		* Get the active tab and call "findEmails()"
-		*/
-		browser.tabs.query({active: true, currentWindow: true})
-		.then(findEmails)
-		.catch(reportError);
-	});
+        /**
+         * Get the active tab and call "findEmails()"
+         */
+        browser.tabs.query({active: true, currentWindow: true})
+            .then(findEmails)
+            .catch(reportError);
+    });
 }
 
 /**
-* There was an error executing the script.
-*/
+ * There was an error executing the script.
+ */
 function reportExecuteScriptError(error) {
-	console.error(`Failed to execute email finder content script: ${error.message}`);
+    console.error(`Failed to execute email finder content script: ${error.message}`);
 }
 
 /**
-* When the popup loads, inject a content script into the active tab,
-* and add a click handler.
-* If we couldn't inject the script, handle the error.
-*/
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
 browser.tabs.executeScript({file: "/content_scripts/email_finder.js"})
-.then(listenForClicks)
-.catch(reportExecuteScriptError);
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
